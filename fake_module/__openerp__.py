4fbc88a60d988290e6ac149f4e0249161caa59ca
Christophe Simonis <christophe@kn.gl>
2014-11-20 11:59:00 +0100

    [ADD] fake_module

---
+++ fake_module/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'fake module',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """A empty module used for tests.""",
+    'author': 'OpenERP SA',
+    'maintainer': 'OpenERP SA',
+    'website': 'https://www.odoo.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
