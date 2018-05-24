c068fb5d4642a6bb1eebd2a9032e4fa201c43adc
David Monjoie <dmo@odoo.com>
2018-05-24 14:56:14 +0200

    [ADD] fake_module_3

---
+++ fake_module_3/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'fake module 3',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """A empty module used for tests.""",
+    'author': 'OpenERP SA',
+    'maintainer': 'OpenERP SA',
+    'website': 'https://www.odoo.com',
+    'depends': ['base', 'fake_module_1'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'price': 50,
+    'currency': 'EUR',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
